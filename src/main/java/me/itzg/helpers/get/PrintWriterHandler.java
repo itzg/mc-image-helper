f7d4361c480e4b94b7fad9cadc7fb8e9dfcbba77
Geoff Bourne <itzgeoff@gmail.com>
2021-10-10 11:57:44 -0500

    fix(get): handled output of large response content

--- src/main/java/me/itzg/helpers/get/PrintWriterHandler.java
+++ src/main/java/me/itzg/helpers/get/PrintWriterHandler.java
@@ -1,11 +1,11 @@
 package me.itzg.helpers.get;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import org.apache.hc.client5.http.impl.classic.AbstractHttpClientResponseHandler;
 import org.apache.hc.core5.http.HttpEntity;
-import org.apache.hc.core5.http.ParseException;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
 
 class PrintWriterHandler extends AbstractHttpClientResponseHandler<String> {
   private final PrintWriter writer;
@@ -16,15 +16,15 @@ class PrintWriterHandler extends AbstractHttpClientResponseHandler<String> {
 
   @Override
   public String handleEntity(HttpEntity entity) throws IOException {
-    try {
 
-      writer.print(
-          EntityUtils.toString(entity)
-      );
-
-    } catch (ParseException e) {
-      throw new IOException("Failed to read entity into string", e);
+    final byte[] buffer = new byte[1024];
+    try (InputStream content = entity.getContent()) {
+      int length;
+      while ((length = content.read(buffer)) >= 0) {
+        writer.print(new String(buffer, 0, length, StandardCharsets.UTF_8));
+      }
     }
+    writer.flush();
 
     // no filename to return
     return "";
