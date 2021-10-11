478adcb9e4eb4bc28793ef58324bd9b4e7cff8fd
Geoff Bourne <itzgeoff@gmail.com>
2021-10-11 17:30:43 -0500

    bug(get): fixed debug log "Post-request capturing..."

--- src/main/java/me/itzg/helpers/get/LatchingUrisInterceptor.java
+++ src/main/java/me/itzg/helpers/get/LatchingUrisInterceptor.java
@@ -1,6 +1,5 @@
 package me.itzg.helpers.get;
 
-import java.io.IOException;
 import java.net.URI;
 import java.util.Deque;
 import java.util.concurrent.ConcurrentLinkedDeque;
@@ -12,17 +11,15 @@ import org.apache.hc.client5.http.classic.ExecChainHandler;
 import org.apache.hc.client5.http.protocol.RedirectLocations;
 import org.apache.hc.core5.http.ClassicHttpRequest;
 import org.apache.hc.core5.http.ClassicHttpResponse;
-import org.apache.hc.core5.http.HttpException;
 
 @Slf4j
 class LatchingUrisInterceptor implements ExecChainHandler {
 
-  private Deque<URI> uris = new ConcurrentLinkedDeque<>();
+  private final Deque<URI> uris = new ConcurrentLinkedDeque<>();
 
   @SneakyThrows
   @Override
-  public ClassicHttpResponse execute(ClassicHttpRequest request, Scope scope, ExecChain chain)
-      throws IOException, HttpException {
+  public ClassicHttpResponse execute(ClassicHttpRequest request, Scope scope, ExecChain chain) {
     log.debug("Intercepting request uri={}", request.getUri());
 
     uris.push(request.getUri());
@@ -30,15 +27,11 @@ class LatchingUrisInterceptor implements ExecChainHandler {
     final ClassicHttpResponse resp = chain.proceed(request, scope);
     final RedirectLocations redirectLocations = scope.clientContext.getRedirectLocations();
     if (redirectLocations != null) {
-      log.debug("Post-request capturing redirectLocations={}", redirectLocations);
+      log.debug("Post-request capturing redirectLocations={}", redirectLocations.getAll());
 
-      redirectLocations.getAll().forEach(uri -> uris.push(uri));
+      redirectLocations.getAll().forEach(uris::push);
     }
     return resp;
-  }
-
-  public Deque<URI> getUris() {
-    return uris;
   }
 
   public URI getLastRequestedUri() {
