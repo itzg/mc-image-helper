da80fc6d1a779aa800712d1b966cd8684e56d7e4
christopher blodgett <christopher.blodgett@gmail.com>
2022-02-02 19:31:21 -0800

    removing unused dependencies

--- src/main/java/me/itzg/helpers/assertcmd/FileExists.java
+++ src/main/java/me/itzg/helpers/assertcmd/FileExists.java
@@ -1,18 +1,11 @@
 package me.itzg.helpers.assertcmd;
 
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.PathMatcher;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.Callable;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.ExitCode;
 import picocli.CommandLine.Parameters;
 import org.apache.tools.ant.DirectoryScanner;
-
 
 @Command(name = "fileExists")
 class FileExists implements Callable<Integer> {
@@ -24,7 +17,6 @@ class FileExists implements Callable<Integer> {
   public Integer call() throws Exception {
     boolean missing = false;
     DirectoryScanner scanner = new DirectoryScanner();
-    //scanner.setBasedir(".");
     scanner.setCaseSensitive(false);
 
     if (paths != null) {
