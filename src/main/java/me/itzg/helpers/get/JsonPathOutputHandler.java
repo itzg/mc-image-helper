eb8dd9fc99549e819d8953a0e7b00d2135a148c9
Geoff Bourne <itzgeoff@gmail.com>
2022-02-26 15:39:11 -0600

    feat(get): configurable behavior when json path is missing (#19)

--- src/main/java/me/itzg/helpers/get/JsonPathOutputHandler.java
+++ src/main/java/me/itzg/helpers/get/JsonPathOutputHandler.java
@@ -1,8 +1,11 @@
 package me.itzg.helpers.get;
 
+import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
-import com.jayway.jsonpath.TypeRef;
+import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.ParseContext;
+import com.jayway.jsonpath.PathNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Path;
@@ -13,28 +16,44 @@ import org.apache.hc.core5.http.HttpEntity;
 
 class JsonPathOutputHandler extends AbstractHttpClientResponseHandler<Path> implements OutputResponseHandler {
 
-  protected static final TypeRef<List<String>> STRING_LIST_TYPE = new TypeRef<List<String>>() {
-  };
   private final PrintWriter writer;
   private final String jsonPath;
+  private final String jsonValueWhenMissing;
+  private final ParseContext parseContext;
 
-  public JsonPathOutputHandler(PrintWriter writer, String jsonPath) {
+  public JsonPathOutputHandler(PrintWriter writer, String jsonPath,
+      String jsonValueWhenMissing) {
     this.writer = writer;
     // adapt jq style path into JsonPath
     this.jsonPath = jsonPath.startsWith("$") ? jsonPath : "$"+jsonPath;
+    this.jsonValueWhenMissing = jsonValueWhenMissing;
+    this.parseContext = JsonPath.using(
+        Configuration.builder()
+            .options(Option.DEFAULT_PATH_LEAF_TO_NULL)
+            .build()
+    );
   }
 
   @Override
   public Path handleEntity(HttpEntity entity) throws IOException {
 
-    final DocumentContext doc = JsonPath.parse(entity.getContent());
+    final DocumentContext doc = parseContext.parse(entity.getContent());
 
     // first try as an atom converted to a string
     String result = doc.read(jsonPath, String.class);
     if (result == null) {
       // might be an indefinite query, so retry as a list
       final List<Object> results = doc.read(jsonPath);
-      if (!results.isEmpty()) {
+      if (results == null) {
+        // leaf is missing
+        if (jsonValueWhenMissing != null) {
+          result = jsonValueWhenMissing;
+        }
+        else {
+          throw new PathNotFoundException("Missing property in path "+jsonPath);
+        }
+      }
+      else if (!results.isEmpty()) {
         result = results.stream()
             .map(Object::toString)
             .collect(Collectors.joining(","));
