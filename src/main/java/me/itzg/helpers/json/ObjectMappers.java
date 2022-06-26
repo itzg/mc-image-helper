0d2596a7de02516e35a83adc055cca184c491357
Geoff Bourne <itzgeoff@gmail.com>
2022-06-26 13:46:36 -0500

    feat(modrinth): limit to mods only for now

--- src/main/java/me/itzg/helpers/json/ObjectMappers.java
+++ src/main/java/me/itzg/helpers/json/ObjectMappers.java
@@ -2,11 +2,13 @@ package me.itzg.helpers.json;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class ObjectMappers {
     private static final ObjectMapper DEFAULT = new ObjectMapper()
         .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
         .registerModule(new JavaTimeModule());
 
     public static ObjectMapper defaultMapper() {
