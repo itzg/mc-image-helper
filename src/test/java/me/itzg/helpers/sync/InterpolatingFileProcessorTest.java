853d7184bf3542d294cc01d91fd786a567e49c55
Jonathan Frederick <doublej472@gmail.com>
2022-12-02 22:28:29 -0600

    Add test for environment variable substitution with Interpolator

--- src/test/java/me/itzg/helpers/sync/InterpolatingFileProcessorTest.java
+++ src/test/java/me/itzg/helpers/sync/InterpolatingFileProcessorTest.java
@@ -1,6 +1,7 @@
 package me.itzg.helpers.sync;
 
 import me.itzg.helpers.env.Interpolator;
+import me.itzg.helpers.env.EnvironmentVariablesProvider;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
@@ -18,12 +19,42 @@ import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.lenient;
 
 @ExtendWith(MockitoExtension.class)
 class InterpolatingFileProcessorTest {
 
     @Mock
     FileProcessor fallbackProcessor;
+
+    @Test
+    void processFileEnv(@TempDir Path tempDir) throws IOException {
+        EnvironmentVariablesProvider environmentVariablesProvider = mock(EnvironmentVariablesProvider.class);
+        // This will be called many times, so be lenient about mismatched mocks
+        lenient().when(environmentVariablesProvider.get("CFG_VELOCITY_SECRET")).thenReturn("SuperSecretValue");
+
+        ReplaceEnvOptions replaceEnvOptions = new ReplaceEnvOptions();
+        replaceEnvOptions.suffixes = Collections.singletonList("yml");
+
+        final Path src_env = Paths.get("src/test/resources/paper-env.yml");
+        final Path src_env_out = Paths.get("src/test/resources/paper-out.yml");
+        final Path dest_env = tempDir.resolve("paper-env.yml");
+
+        final InterpolatingFileProcessor processor = new InterpolatingFileProcessor(
+                replaceEnvOptions,
+                new Interpolator(environmentVariablesProvider, "CFG_"),
+                fallbackProcessor
+        );
+
+        processor.processFile(src_env, dest_env);
+
+        assertThat(dest_env).exists();
+        assertThat(dest_env).hasSameTextualContentAs(src_env_out, StandardCharsets.UTF_8);
+
+        verifyNoInteractions(fallbackProcessor);
+    }
 
     @Test
     void processFile(@TempDir Path tempDir) throws IOException {
