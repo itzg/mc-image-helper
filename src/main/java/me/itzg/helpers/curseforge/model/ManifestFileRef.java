8f2d7ac675d563cd3436a77861384a46b6adae16
Geoff Bourne <itzgeoff@gmail.com>
2023-01-11 22:23:23 -0600

    Revert "curseforge: include only server mods (#89)"
    This reverts commit e3df1769569412d3b2ec14f8fd2869fe1c8ec887.

--- src/main/java/me/itzg/helpers/curseforge/model/ManifestFileRef.java
+++ src/main/java/me/itzg/helpers/curseforge/model/ManifestFileRef.java
@@ -1,12 +1,10 @@
 package me.itzg.helpers.curseforge.model;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Data
 public class ManifestFileRef {
     private int projectID;
-    private int fileID;
-    @EqualsAndHashCode.Exclude
     private boolean required;
+    private int fileID;
 }
