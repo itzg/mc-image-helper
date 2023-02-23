bad4d2c5295a24cd56dfa3d5ccce056b57a7b431
Geoff Bourne <itzgeoff@gmail.com>
2023-02-23 08:56:51 -0600

    curseforge: ensure modpack-file listing is not out-dated (#147)

--- src/main/java/me/itzg/helpers/curseforge/CurseForgeInstaller.java
+++ src/main/java/me/itzg/helpers/curseforge/CurseForgeInstaller.java
@@ -248,7 +248,9 @@ public class CurseForgeInstaller {
     ) {
         // NOTE latestFiles in mod is only one or two files, so retrieve the full list instead
         final GetModFilesResponse resp = preparedFetch.fetch(
-                uriBuilder.resolve("/mods/{modId}/files", mod.getId())
+                // 'latest' parameter is not recognized by the API but rather used to force it not to return
+                // a cached listing
+                uriBuilder.resolve("/mods/{modId}/files?latest={ts}", mod.getId(), System.currentTimeMillis())
             )
             .toObject(GetModFilesResponse.class)
             .execute();
