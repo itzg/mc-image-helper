5f40b4dae8497d06431efb6d57bb5ce56543fdac
Geoff Bourne <itzgeoff@gmail.com>
2023-03-16 22:13:04 -0500

    forge: extract version from provided installer file

--- src/main/java/me/itzg/helpers/files/IoStreams.java
+++ src/main/java/me/itzg/helpers/files/IoStreams.java
@@ -3,6 +3,10 @@ package me.itzg.helpers.files;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
  * Since Java 8 doesn't have InputStream.transferTo
@@ -15,6 +19,23 @@ public class IoStreams {
         while ((length = in.read(buf)) != -1) {
            out.write(buf, 0, length);
         }
+    }
 
+    @FunctionalInterface
+    public interface EntryReader<T> {
+        T read(InputStream in) throws IOException;
+    }
+
+    public static <T> T readFileFromZip(Path zipFile, String entryName, EntryReader<T> reader) throws IOException {
+        try (ZipInputStream zipIn = new ZipInputStream(Files.newInputStream(zipFile))) {
+            ZipEntry entry;
+            while ((entry = zipIn.getNextEntry()) != null) {
+                if (entry.getName().equals(entryName)) {
+                    return reader.read(zipIn);
+                }
+            }
+        }
+
+        return null;
     }
 }
