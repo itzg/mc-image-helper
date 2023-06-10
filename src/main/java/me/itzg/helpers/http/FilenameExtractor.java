8ebf3db16f49d864ad9246485889d2d4065c4bf3
Geoff Bourne <itzgeoff@gmail.com>
2023-06-09 21:59:13 -0500

    Improve content disposition handling of filename*

--- src/main/java/me/itzg/helpers/http/FilenameExtractor.java
+++ src/main/java/me/itzg/helpers/http/FilenameExtractor.java
@@ -1,18 +1,23 @@
 package me.itzg.helpers.http;
 
-import lombok.extern.slf4j.Slf4j;
-import org.apache.hc.client5.http.HttpResponseException;
-import org.apache.hc.core5.http.*;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
-
 import java.io.IOException;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import lombok.extern.slf4j.Slf4j;
+import me.itzg.helpers.errors.GenericException;
+import org.apache.hc.client5.http.HttpResponseException;
+import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.Header;
+import org.apache.hc.core5.http.HttpStatus;
+import org.apache.hc.core5.http.ProtocolException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
 
 @Slf4j
 public class FilenameExtractor {
     private static final Pattern RFC_2047_ENCODED = Pattern.compile("=\\?UTF-8\\?Q\\?(.+)\\?=");
+    private static final Pattern RFC_5987_ENCODED = Pattern.compile("UTF-8''(.+)");
 
   private final LatchingUrisInterceptor interceptor;
 
@@ -27,12 +32,29 @@ public class FilenameExtractor {
 
       final ContentType parsed = ContentType.parse(headerValue);
       log.debug("Response has contentDisposition={}", headerValue);
+      final String filenameStar = parsed.getParameter("filename*");
       final String filename = parsed.getParameter("filename");
-      final Matcher m = RFC_2047_ENCODED.matcher(filename);
-      if (m.matches()) {
-          return m.group(1);
+      if (filenameStar != null) {
+          final Matcher m = RFC_5987_ENCODED.matcher(filenameStar);
+          if (m.matches()) {
+              return m.group(1);
+          }
+          else {
+              throw new GenericException("Invalid RFC 5987 encoding of filename* from " + filenameStar);
+          }
       }
-      return filename;
+      else if (filename != null) {
+          final Matcher m = RFC_2047_ENCODED.matcher(filename);
+          if (m.matches()) {
+              return m.group(1);
+          }
+          else {
+              throw new GenericException("Invalid RFC 2047 encoding of filename from " + filename);
+          }
+      }
+      else {
+          throw new GenericException("Unable to determine filename from header: " + headerValue);
+      }
   }
 
   public String extract(ClassicHttpResponse response) throws IOException, ProtocolException {
