cfbc8e462426f302e153118462a38be7b65aab7f
Kyle Smith <kyle@kmaismith.com>
2023-08-04 23:06:28 -0400

    InstallModrinthModpackCommand uses Fetcher and Installer classes

--- src/main/java/me/itzg/helpers/modrinth/InstallModrinthModpackCommand.java
+++ src/main/java/me/itzg/helpers/modrinth/InstallModrinthModpackCommand.java
@@ -101,271 +101,42 @@ public class InstallModrinthModpackCommand implements Callable<Integer> {
 
     @Override
     public Integer call() throws IOException {
+        final ModrinthApiClient apiClient = new ModrinthApiClient(
+            baseUrl, "install-modrinth-modpack", sharedFetchArgs.options());
+
         final ModrinthModpackManifest prevManifest = Manifests.load(
-            outputDirectory, ModrinthModpackManifest.ID, ModrinthModpackManifest.class);
+            outputDirectory, ModrinthModpackManifest.ID,
+             ModrinthModpackManifest.class);
 
-        final ProjectRef projectRef;
-        final Matcher m = MODPACK_PAGE_URL.matcher(modpackProject);
-        if (m.matches()) {
-            final String versionName = m.group("versionName");
-            if (versionName != null && version != null) {
-                throw new InvalidParameterException("Cannot provide both project file URL and version");
-            }
-            projectRef = new ProjectRef(m.group("slug"), versionName != null ? versionName : version);
-        } else {
-            projectRef = new ProjectRef(modpackProject, version);
-        }
+        final ProjectRef projectRef =
+            ProjectRef.fromPossibleUrl(modpackProject, version);
 
-        final ModrinthModpackManifest newManifest =
-            processModpack(projectRef, prevManifest);
-
-        if (newManifest != null) {
-            Manifests.cleanup(outputDirectory, prevManifest, newManifest, log);
-            Manifests.save(outputDirectory, ModrinthModpackManifest.ID, newManifest);
-        }
+        new ModrinthApiPackFetcher(
+            apiClient, projectRef, this.outputDirectory, this.gameVersion,
+            this.defaultVersionType, this.loader.asLoader())
+            .fetchModpack(prevManifest)
+            .flatMap(archivePath ->
+                new ModrinthPackInstaller(apiClient, this.sharedFetchArgs,
+                    archivePath, this.outputDirectory, this.resultsFile,
+                    this.forceModloaderReinstall)
+                .processModpack())
+            .flatMap(installation ->
+                Mono.just(ModrinthModpackManifest.builder()
+                    .files(Manifests.relativizeAll(this.outputDirectory, installation.files))
+                    .projectSlug(projectRef.getIdOrSlug())
+                    .versionId(projectRef.getVersionId())
+                    .dependencies(installation.index.getDependencies())
+                    .build()))
+            .handle((newManifest, sink) -> {
+                try {
+                    Manifests.cleanup(this.outputDirectory, prevManifest, newManifest, log);
+                    Manifests.save(outputDirectory, ModrinthModpackManifest.ID, newManifest);
+                } catch (IOException e) {
+                    sink.error(e);
+                }
+            })
+            .block();
 
         return ExitCode.OK;
     }
-
-    /**
-     * @return the new manifest or null if already installed
-     */
-    private ModrinthModpackManifest processModpack(ProjectRef projectRef, ModrinthModpackManifest prevManifest) {
-
-        try (ModrinthApiClient apiClient = new ModrinthApiClient(baseUrl, "install-modrinth-modpack", sharedFetchArgs.options())) {
-            return apiClient.getProject(projectRef.getIdOrSlug())
-                .onErrorMap(FailedRequestException::isNotFound,
-                    throwable ->
-                        new InvalidParameterException("Unable to locate requested project given " + projectRef, throwable)
-                )
-                .flatMap(project ->
-                    apiClient.resolveProjectVersion(
-                            project, projectRef, loader != null ? loader.asLoader() : null, gameVersion, defaultVersionType
-                        )
-                        .switchIfEmpty(Mono.defer(() -> Mono.error(new InvalidParameterException(
-                            "Unable to find version given " + projectRef)))
-                        )
-                        .doOnNext(version -> log.debug("Resolved version={} from projectRef={}", version.getVersionNumber(), projectRef))
-                        .publishOn(Schedulers.boundedElastic()) // since next item does I/O
-                        .filter(version -> needsInstall(prevManifest, project, version))
-                        .flatMap(version -> processVersion(apiClient, project, version))
-                        .switchIfEmpty(Mono.defer(() -> {
-                            try {
-                                applyModLoader(prevManifest.getDependencies());
-                            } catch (IOException e) {
-                                return Mono.error(new GenericException("Failed to re-apply mod loader", e));
-                            }
-                            return Mono.just(prevManifest);
-                        }))
-                )
-                .block();
-
-        }
-    }
-
-    @NotNull
-    private Mono<ModrinthModpackManifest> processVersion(ModrinthApiClient apiClient, Project project,
-        Version version
-    ) {
-        final VersionFile versionFile = pickVersionFile(version);
-        log.info("Installing version {} of {}", version.getVersionNumber(), project.getTitle());
-        //noinspection BlockingMethodInNonBlockingContext because IntelliJ is confused
-        return apiClient.downloadMrPack(versionFile)
-            .publishOn(Schedulers.boundedElastic())
-            .flatMap(zipPath ->
-                processModpackZip(apiClient, zipPath, project, version)
-                    .publishOn(Schedulers.boundedElastic())
-                    .doOnTerminate(() -> {
-                        try {
-                            Files.delete(zipPath);
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    })
-            );
-    }
-
-    @Blocking
-    private boolean needsInstall(ModrinthModpackManifest prevManifest, Project project, Version version) {
-        if (prevManifest != null) {
-            if (prevManifest.getProjectSlug().equals(project.getSlug())
-                && prevManifest.getVersionId().equals(version.getId())
-                && prevManifest.getDependencies() != null
-                && Manifests.allFilesPresent(outputDirectory, prevManifest)
-            ) {
-                if (forceSynchronize) {
-                    log.info("Requested force synchronize of {}", project.getTitle());
-                } else {
-                    log.info("Modpack {} version {} is already installed",
-                        project.getTitle(), version.getName()
-                    );
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    @Blocking
-    private Mono<ModrinthModpackManifest> processModpackZip(ModrinthApiClient apiClient, Path zipFile, Project project,
-        Version version
-    ) {
-        final ModpackIndex modpackIndex;
-        try {
-            modpackIndex = IoStreams.readFileFromZip(zipFile, "modrinth.index.json", in ->
-                ObjectMappers.defaultMapper().readValue(in, ModpackIndex.class)
-            );
-        } catch (IOException e) {
-            return Mono.error(new GenericException("Failed to read modpack index", e));
-        }
-
-        if (modpackIndex == null) {
-            return Mono.error(
-                new InvalidParameterException("Modpack is missing modrinth.index.json")
-            );
-        }
-
-        if (!Objects.equals("minecraft", modpackIndex.getGame())) {
-            return Mono.error(
-                new InvalidParameterException("Requested modpack is not for minecraft: " + modpackIndex.getGame()));
-        }
-
-        return processModpackFiles(apiClient, modpackIndex)
-            .collectList()
-            .map(modFiles ->
-                Stream.of(
-                        modFiles.stream(),
-                        extractOverrides(zipFile, "overrides", "server-overrides")
-                    )
-                    .flatMap(Function.identity())
-                    .collect(Collectors.toList())
-            )
-            .flatMap(paths -> {
-                try {
-                    applyModLoader(modpackIndex.getDependencies());
-                } catch (IOException e) {
-                    return Mono.error(new GenericException("Failed to apply mod loader", e));
-                }
-
-                return Mono.just(ModrinthModpackManifest.builder()
-                    .files(Manifests.relativizeAll(outputDirectory, paths))
-                    .projectSlug(project.getSlug())
-                    .versionId(version.getId())
-                    .dependencies(modpackIndex.getDependencies())
-                    .build());
-            });
-    }
-
-    private void applyModLoader(Map<DependencyId, String> dependencies) throws IOException {
-        log.debug("Applying mod loader from dependencies={}", dependencies);
-
-        final String minecraftVersion = dependencies.get(DependencyId.minecraft);
-        if (minecraftVersion == null) {
-            throw new GenericException("Modpack dependencies missing minecraft version: " + dependencies);
-        }
-
-        final String forgeVersion = dependencies.get(DependencyId.forge);
-        if (forgeVersion != null) {
-            new ForgeInstaller().install(
-                minecraftVersion,
-                forgeVersion,
-                outputDirectory,
-                resultsFile,
-                forceModloaderReinstall,
-                null
-            );
-            return;
-        }
-
-        final String fabricVersion = dependencies.get(DependencyId.fabricLoader);
-        if (fabricVersion != null) {
-            new FabricLauncherInstaller(outputDirectory)
-                .setResultsFile(resultsFile)
-                .installUsingVersions(
-                    minecraftVersion,
-                    fabricVersion,
-                    null
-                );
-            return;
-        }
-
-        final String quiltVersion = dependencies.get(DependencyId.quiltLoader);
-        if (quiltVersion != null) {
-            try (QuiltInstaller installer = new QuiltInstaller(QuiltInstaller.DEFAULT_REPO_URL,
-                sharedFetchArgs.options(),
-                outputDirectory,
-                minecraftVersion
-            )
-                .setResultsFile(resultsFile)) {
-
-                installer.installWithVersion(null, quiltVersion);
-            }
-        }
-
-    }
-
-    @SuppressWarnings("SameParameterValue")
-    private Stream<Path> extractOverrides(Path zipFile, String... overridesDirs) {
-        return Stream.of(overridesDirs)
-            .flatMap(dir ->
-            {
-                final String prefix = dir + "/";
-                final List<Path> extracted = new ArrayList<>();
-                try (ZipInputStream zipIn = new ZipInputStream(Files.newInputStream(zipFile))) {
-                    ZipEntry entry;
-                    while ((entry = zipIn.getNextEntry()) != null) {
-                        if (!entry.isDirectory()) {
-                            if (entry.getName().startsWith(prefix)) {
-                                final Path outFile = outputDirectory.resolve(
-                                    entry.getName().substring(prefix.length())
-                                );
-                                Files.createDirectories(outFile.getParent());
-
-                                try {
-                                    Files.copy(zipIn, outFile, StandardCopyOption.REPLACE_EXISTING);
-                                    extracted.add(outFile);
-                                } catch (IOException e) {
-                                    throw new GenericException(
-                                        String.format("Failed to extract %s from overrides", entry.getName()), e
-                                    );
-                                }
-                            }
-                        }
-                    }
-                } catch (IOException e) {
-                    throw new GenericException("Failed to extract overrides", e);
-                }
-                return extracted.stream();
-            });
-    }
-
-    private Flux<Path> processModpackFiles(ModrinthApiClient apiClient, ModpackIndex modpackIndex) {
-        return Flux.fromStream(modpackIndex.getFiles().stream()
-                .filter(modpackFile ->
-                    // env is optional
-                    modpackFile.getEnv() == null
-                        || modpackFile.getEnv().get(Env.server) != EnvType.unsupported
-                )
-            )
-            .publishOn(Schedulers.boundedElastic())
-            .flatMap(modpackFile ->
-                {
-                    final Path outFilePath = outputDirectory.resolve(modpackFile.getPath());
-                    try {
-                        //noinspection BlockingMethodInNonBlockingContext
-                        Files.createDirectories(outFilePath.getParent());
-                    } catch (IOException e) {
-                        return Mono.error(new GenericException("Failed to created directory for file to download", e));
-                    }
-
-                    return apiClient.downloadFileFromUrl(
-                        outFilePath,
-                        modpackFile.getDownloads().get(0),
-                        (uri, file, contentSizeBytes) -> log.info("Downloaded {}", modpackFile.getPath())
-                    );
-                }
-            );
-    }
-
-
 }
