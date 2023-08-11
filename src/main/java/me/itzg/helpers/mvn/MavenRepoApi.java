daaa6fbd5e58fb00b3866877904a323ca69dde99
Geoff Bourne <itzgeoff@gmail.com>
2023-08-10 19:53:50 -0500

    mvn: cache metadata retrieval

--- src/main/java/me/itzg/helpers/mvn/MavenRepoApi.java
+++ src/main/java/me/itzg/helpers/mvn/MavenRepoApi.java
@@ -1,15 +1,41 @@
 package me.itzg.helpers.mvn;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.TemporalAmount;
+import java.util.function.Supplier;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import me.itzg.helpers.errors.GenericException;
 import me.itzg.helpers.http.SharedFetch;
 import me.itzg.helpers.http.UriBuilder;
+import org.jetbrains.annotations.NotNull;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
+@Slf4j
 public class MavenRepoApi {
+
+    public static final TemporalAmount MAX_CACHE_AGE = Duration.ofHours(1);
+
     private final UriBuilder uriBuilder;
     private final SharedFetch sharedFetch;
     private final XmlMapper xmlMapper;
+    @Setter
+    private Path metadataCacheDir;
+    @Setter
+    private boolean skipExisting;
+    @Setter
+    private boolean skipUpToDate;
+    @Setter
+    private Supplier<Instant> instantSource = Instant::now;
 
     public MavenRepoApi(String repoUrl, SharedFetch sharedFetch) {
         uriBuilder = UriBuilder.withBaseUrl(repoUrl);
@@ -18,6 +44,23 @@ public class MavenRepoApi {
     }
 
     public Mono<MavenMetadata> fetchMetadata(String groupId, String artifactId) {
+        final Mono<MavenMetadata> cachedMono = loadFromCache(groupId, artifactId);
+
+        return cachedMono
+            .switchIfEmpty(retrieveMetadata(groupId, artifactId)
+                .flatMap(mavenMetadata -> {
+                    if (metadataCacheDir != null) {
+                        return cacheMetadata(groupId, artifactId, mavenMetadata);
+                    }
+                    else {
+                        return Mono.just(mavenMetadata);
+                    }
+                })
+            );
+    }
+
+    @NotNull
+    private Mono<MavenMetadata> retrieveMetadata(String groupId, String artifactId) {
         final String groupPath = groupId.replace('.', '/');
 
         return sharedFetch
@@ -29,6 +72,60 @@ public class MavenRepoApi {
             .toObject(MavenMetadata.class, xmlMapper)
             .assemble()
             .checkpoint(String.format("fetching metadata for %s:%s", groupId, artifactId));
+    }
+
+    @NotNull
+    private Mono<MavenMetadata> cacheMetadata(String groupId, String artifactId, MavenMetadata mavenMetadata) {
+        return Mono.fromSupplier(() -> {
+                try {
+                    final Path dir = Files.createDirectories(metadataCacheDir);
+                    final Path metadataFile = resolveMetadataFile(groupId, artifactId, dir);
+                    log.debug("Caching {}:{} metadata to {}", groupId, artifactId, metadataFile);
+                    xmlMapper.writeValue(metadataFile.toFile(), mavenMetadata);
+                } catch (URISyntaxException | IOException e) {
+                    throw new GenericException("Caching maven metadata", e);
+                }
+                return mavenMetadata;
+            })
+            .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    /**
+     * @return cached file or empty is absent or too old
+     */
+    @NotNull
+    private Mono<MavenMetadata> loadFromCache(String groupId, String artifactId) {
+        final Mono<MavenMetadata> cachedMono;
+        if (metadataCacheDir != null) {
+            cachedMono = Mono.defer(() -> {
+                try {
+                    final Path metadataFile = resolveMetadataFile(groupId, artifactId, metadataCacheDir);
+                    if (Files.exists(metadataFile)) {
+                        @SuppressWarnings("BlockingMethodInNonBlockingContext") // has subscribeOn below
+                        final FileTime modifiedTime = Files.getLastModifiedTime(metadataFile);
+                        log.debug("Metadata cache file exists at={} modified={}", metadataFile, modifiedTime);
+                        if (modifiedTime.toInstant().isAfter(instantSource.get().minus(MAX_CACHE_AGE))) {
+                            return Mono.just(xmlMapper.readValue(metadataFile.toFile(), MavenMetadata.class));
+                        }
+                    }
+                    return Mono.empty();
+                } catch (URISyntaxException | IOException e) {
+                    return Mono.error(new GenericException("Reading metadata cache", e));
+                }
+            })
+                .subscribeOn(Schedulers.boundedElastic());
+        }
+        else {
+            cachedMono = Mono.empty();
+        }
+        return cachedMono;
+    }
+
+    @NotNull
+    private Path resolveMetadataFile(String groupId, String artifactId, Path dir) throws URISyntaxException {
+        final URI uri = new URI(uriBuilder.getBaseUrl());
+        final String host = uri.getHost();
+        return dir.resolve(String.format("%s-%s-%s.xml", host, groupId, artifactId));
     }
 
     /**
@@ -63,7 +160,8 @@ public class MavenRepoApi {
                         )
                     )
                     .toFile(outputDirectory.resolve(filename))
-                    .skipUpToDate(true)
+                    .skipExisting(skipExisting)
+                    .skipUpToDate(skipUpToDate)
                     .assemble()
                     .checkpoint(String.format("Downloading %s:%s:%s%s.%s",
                         groupId, artifactId, resolvedVersion, classifier != null ? ":"+classifier : "",
