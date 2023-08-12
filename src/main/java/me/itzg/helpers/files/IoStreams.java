d5fa6a94c3f1a605e03797c7c90b9e31acdbab5f
Geoff Bourne <itzgeoff@gmail.com>
2023-08-12 09:30:31 -0500

    modrinth: resolve "only DEFLATED entries can have EXT" (#286)

--- src/main/java/me/itzg/helpers/files/IoStreams.java
+++ src/main/java/me/itzg/helpers/files/IoStreams.java
@@ -2,10 +2,9 @@ package me.itzg.helpers.files;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
+import java.util.zip.ZipFile;
+import me.itzg.helpers.errors.GenericException;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -23,15 +22,18 @@ public class IoStreams {
      */
     @Nullable
     public static <T> T readFileFromZip(Path zipFile, String entryName, EntryReader<T> entryReader) throws IOException {
-        try (ZipInputStream zipIn = new ZipInputStream(Files.newInputStream(zipFile))) {
-            ZipEntry entry;
-            while ((entry = zipIn.getNextEntry()) != null) {
-                if (entry.getName().equals(entryName)) {
-                    return entryReader.read(zipIn);
-                }
-            }
+        try (ZipFile zipFileReader = new ZipFile(zipFile.toFile())) {
+            return zipFileReader.stream()
+                .filter(zipEntry -> zipEntry.getName().equals(entryName))
+                .findFirst()
+                .map(zipEntry -> {
+                    try {
+                        return entryReader.read(zipFileReader.getInputStream(zipEntry));
+                    } catch (IOException e) {
+                        throw new GenericException("Getting entry input stream from zip file", e);
+                    }
+                })
+                .orElse(null);
         }
-
-        return null;
     }
 }
