dc54dda49d29c933117b353c08a59288ffa764fb
Geoff Bourne <itzgeoff@gmail.com>
2023-09-30 11:11:52 -0500

    java-release: support >= 21 (#316)

--- src/main/java/me/itzg/helpers/versions/JavaReleaseCommand.java
+++ src/main/java/me/itzg/helpers/versions/JavaReleaseCommand.java
@@ -1,38 +1,36 @@
 package me.itzg.helpers.versions;
 
 import java.util.concurrent.Callable;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.ExitCode;
 
 @Command(name = "java-release", description = "Outputs the Java release number, such as 8, 11, 17")
 public class JavaReleaseCommand implements Callable<Integer> {
 
-  @Override
-  public Integer call() throws Exception {
-    final String versionStr = System.getProperty("java.version");
+    @Override
+    public Integer call() throws Exception {
+        final String versionStr = System.getProperty("java.version");
 
-    final Pattern pattern = Pattern.compile("(\\d+)\\.(\\d+)");
-    final Matcher m = pattern.matcher(versionStr);
-    if (m.lookingAt()) {
-      final int major = Integer.parseInt(m.group(1));
-      final int minor = Integer.parseInt(m.group(2));
-      if (major == 1) {
-        System.out.println(minor);
-      } else if (major >= 9) {
-        System.out.println(major);
-      }
-      else {
-        System.err.println("Unexpected version: "+versionStr);
-        return ExitCode.SOFTWARE;
-      }
-    }
-    else {
-      System.err.println("Malformed version: "+versionStr);
-      return ExitCode.SOFTWARE;
-    }
+        final String[] parts = versionStr.split("\\.");
 
-    return ExitCode.OK;
-  }
+        try {
+            final int major = Integer.parseInt(parts[0]);
+            final Integer minor = parts.length >= 2 ? Integer.parseInt(parts[1]) : null;
+            if (major == 1 && minor != null) {
+                System.out.println(minor);
+            }
+            else if (major >= 9) {
+                System.out.println(major);
+            }
+            else {
+                System.err.println("Unexpected version: " + versionStr);
+                return ExitCode.SOFTWARE;
+            }
+        } catch (NumberFormatException e) {
+            System.err.println("Malformed version: " + versionStr);
+            return ExitCode.SOFTWARE;
+        }
+
+        return ExitCode.OK;
+    }
 }
