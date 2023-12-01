32ad5611f1f478d0a1e0792ee40a07d58046e4c3
Geoff Bourne <itzgeoff@gmail.com>
2023-12-01 11:58:00 -0600

    build: downgrade httpclient5 due to buggy release 5.2.2

--- src/test/java/me/itzg/helpers/get/GetCommandTest.java
+++ src/test/java/me/itzg/helpers/get/GetCommandTest.java
@@ -1,221 +1,225 @@
 package me.itzg.helpers.get;
 
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemErrNormalized;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockserver.model.HttpRequest.request;
-import static org.mockserver.model.HttpResponse.response;
 
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import com.github.tomakehurst.wiremock.stubbing.Scenario;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import me.itzg.helpers.TestLoggingAppender;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockserver.integration.ClientAndServer;
-import org.mockserver.junit.jupiter.MockServerExtension;
-import org.mockserver.model.MediaType;
-import org.mockserver.verify.VerificationTimes;
 import picocli.CommandLine;
+import picocli.CommandLine.ExitCode;
 
-@ExtendWith(MockServerExtension.class)
+@WireMockTest(httpsEnabled = true)
 class GetCommandTest {
-  private final ClientAndServer client;
-  private final MockServerSupport mock;
 
-  GetCommandTest(ClientAndServer client) {
-    this.client = client;
-    this.mock = new MockServerSupport(client);
-  }
+    @AfterEach
+    void tearDown() {
+        TestLoggingAppender.reset();
+    }
 
-  @AfterEach
-  void tearDown() {
-    client.reset();
-    TestLoggingAppender.reset();
-  }
+    @Test
+    void outputsDownload(WireMockRuntimeInfo wmInfo) throws MalformedURLException {
+        stubFor(get("/outputsDownload.txt")
+            .willReturn(aResponse()
+                .withHeader("Content-Type", "text/plain")
+                .withBody("Response content")
+            )
+        );
 
-  @Test
-  void outputsDownload() throws MalformedURLException {
-    mock.expectRequest("GET","/outputsDownload.txt",
-        response()
-            .withBody("Response content", MediaType.TEXT_PLAIN)
-    );
+        final StringWriter output = new StringWriter();
+        final int status =
+            new CommandLine(new GetCommand())
+                .setOut(new PrintWriter(output))
+                .execute(
+                    wmInfo.getHttpBaseUrl() + "/outputsDownload.txt"
+                );
 
-    final StringWriter output = new StringWriter();
-    final int status =
-        new CommandLine(new GetCommand())
-            .setOut(new PrintWriter(output))
-            .execute(
-                mock.buildMockedUrl("/outputsDownload.txt").toString()
-            );
+        assertThat(status).isEqualTo(0);
+        assertThat(output.toString()).isEqualTo("Response content");
+    }
 
-    assertThat(status).isEqualTo(0);
-    assertThat(output.toString()).isEqualTo("Response content");
-  }
+    @Test
+    void usesBasicAuth(WireMockRuntimeInfo wmInfo) throws MalformedURLException, URISyntaxException {
+        stubFor(get("/").withBasicAuth("user", "pass")
+            .willReturn(aResponse()
+                .withBody("You're in")
+            )
+        );
 
-  @Test
-  void usesBasicAuth() throws MalformedURLException, URISyntaxException {
-    mock.expectRequest("GET","/",
-        request -> request.withHeader("Authorization", "Basic dXNlcjpwYXNz"),
-        response()
-            .withBody("You're in", MediaType.TEXT_PLAIN)
-    );
+        final URI baseUri = URI.create(wmInfo.getHttpBaseUrl());
 
-    final StringWriter output = new StringWriter();
-    final int status =
-        new CommandLine(new GetCommand())
-            .setOut(new PrintWriter(output))
-            .execute(
-                mock.buildMockedUrl("/", "user:pass").toString()
-            );
+        final StringWriter output = new StringWriter();
+        final int status =
+            new CommandLine(new GetCommand())
+                .setOut(new PrintWriter(output))
+                .execute(
+                    new URI(baseUri.getScheme(), "user:pass", baseUri.getHost(), baseUri.getPort(), "/", null, null).toString()
+                );
 
-    assertThat(status).isEqualTo(0);
-    assertThat(output.toString()).isEqualTo("You're in");
-  }
+        assertThat(status).isEqualTo(0);
+        assertThat(output.toString()).isEqualTo("You're in");
+    }
 
-  @Test
-  void handlesExtraSlashAtStartOfPath() throws MalformedURLException {
-    mock.expectRequest("GET","/handlesExtraSlashAtStartOfPath.txt",
-        response()
-            .withBody("Response content", MediaType.TEXT_PLAIN)
-    );
+    @Test
+    void handlesExtraSlashAtStartOfPath(WireMockRuntimeInfo wmInfo) throws MalformedURLException {
+        stubFor(get("/handlesExtraSlashAtStartOfPath.txt")
+            .willReturn(aResponse()
+                .withBody("Response content")
+            )
+        );
 
-    final StringWriter output = new StringWriter();
-    final int status =
-        new CommandLine(new GetCommand())
-            .setOut(new PrintWriter(output))
-            .execute(
-                mock.buildMockedUrl("//handlesExtraSlashAtStartOfPath.txt").toString()
-            );
+        final StringWriter output = new StringWriter();
+        final int status =
+            new CommandLine(new GetCommand())
+                .setOut(new PrintWriter(output))
+                .execute(
+                    wmInfo.getHttpBaseUrl() + "//handlesExtraSlashAtStartOfPath.txt"
+                );
 
-    assertThat(status).isEqualTo(0);
-    assertThat(output.toString()).isEqualTo("Response content");
-  }
+        assertThat(status).isEqualTo(0);
+        assertThat(output.toString()).isEqualTo("Response content");
+    }
 
-  @Test
-  void handlesNotFound() throws MalformedURLException {
-    mock.expectRequest("GET","/handlesNotFound",
-        response()
-            .withStatusCode(404)
-            .withBody("<html><body>Not found</body></html>", MediaType.TEXT_HTML_UTF_8), 
-        5
-    );
+    @Test
+    void handlesNotFound(WireMockRuntimeInfo wmInfo) throws MalformedURLException {
+        stubFor(get("/handlesNotFound")
+            .willReturn(aResponse()
+                .withStatus(404)
+                .withHeader("Content-Type", "text/html")
+                .withBody("<html><body>Not found</body></html>")
+            )
+        );
 
-    final StringWriter output = new StringWriter();
-    final int status =
-        new CommandLine(new GetCommand())
-            .setOut(new PrintWriter(output))
-            .execute(
-                mock.buildMockedUrl("/handlesNotFound").toString()
-            );
+        final StringWriter output = new StringWriter();
+        final int status =
+            new CommandLine(new GetCommand())
+                .setOut(new PrintWriter(output))
+                .execute(
+                    wmInfo.getHttpBaseUrl() + "/handlesNotFound"
+                );
 
-    assertThat(status).isEqualTo(1);
-    assertThat(output.toString()).isEqualTo("");
+        assertThat(status).isEqualTo(1);
+        assertThat(output.toString()).isEqualTo("");
 
-  }
+    }
 
-  @Test
-  void handlesRetryOn403ThenSuccess() throws MalformedURLException {
-    mock.expectRequest("GET","/handlesRetry",
-        response()
-            .withStatusCode(403)
-            .withBody("Permission denied", MediaType.TEXT_PLAIN),
-        1
-    );
-    mock.expectRequest("GET","/handlesRetry",
-        response()
-            .withStatusCode(200)
-            .withBody("Success", MediaType.TEXT_PLAIN),
-        1
-    );
+    @Test
+    void handlesRetryOn403ThenSuccess(WireMockRuntimeInfo wmInfo) throws MalformedURLException {
+        stubFor(get("/handlesRetry").inScenario("retry")
+            .whenScenarioStateIs(Scenario.STARTED)
+            .willReturn(aResponse().withStatus(403))
+        )
+            .setNewScenarioState("did 403");
+        stubFor(get("/handlesRetry").inScenario("retry")
+            .whenScenarioStateIs("did 403")
+            .willReturn(aResponse().withBody("Success"))
+        );
 
-    final StringWriter output = new StringWriter();
-    final int status =
-        new CommandLine(new GetCommand())
-            .setOut(new PrintWriter(output))
-            .execute(
-                "--retry-count=1",
-                "--retry-delay=1",
-                mock.buildMockedUrl("/handlesRetry").toString()
-            );
+        final StringWriter output = new StringWriter();
+        final int status =
+            new CommandLine(new GetCommand())
+                .setOut(new PrintWriter(output))
+                .execute(
+                    "--retry-count=1",
+                    "--retry-delay=1",
+                    wmInfo.getHttpBaseUrl() + "/handlesRetry"
+                );
 
-    assertThat(status).isEqualTo(0);
-    assertThat(output.toString()).isEqualTo("Success");
+        assertThat(status).isEqualTo(0);
+        assertThat(output.toString()).isEqualTo("Success");
 
-    client.verify(
-        request("/handlesRetry"),
-        VerificationTimes.exactly(2)
-    );
-  }
+        verify(2, getRequestedFor(urlEqualTo("/handlesRetry")));
+    }
 
-  @Test
-  void handlesRetryThenFails() throws MalformedURLException {
-    mock.expectRequest("GET","/handlesRetry",
-        response()
-            .withStatusCode(403)
-            .withBody("Permission denied", MediaType.TEXT_PLAIN),
-        2
-    );
+    @Test
+    void handlesRetryThenFails(WireMockRuntimeInfo wmInfo) throws MalformedURLException {
+        stubFor(get("/handlesRetry")
+            .willReturn(aResponse().withStatus(403))
+        );
 
-    final StringWriter output = new StringWriter();
-    final int status =
-        new CommandLine(new GetCommand())
-            .setOut(new PrintWriter(output))
-            .execute(
-                "--retry-count=1",
-                "--retry-delay=1",
-                mock.buildMockedUrl("/handlesRetry").toString()
-            );
+        final StringWriter output = new StringWriter();
+        final int status =
+            new CommandLine(new GetCommand())
+                .setOut(new PrintWriter(output))
+                .execute(
+                    "--retry-count=1",
+                    "--retry-delay=1",
+                    wmInfo.getHttpBaseUrl() + "/handlesRetry"
+                );
 
-    assertThat(status).isEqualTo(1);
+        assertThat(status).isEqualTo(1);
 
-    client.verify(
-        request("/handlesRetry"),
-        VerificationTimes.exactly(2)
-    );
-  }
+        verify(2, getRequestedFor(urlEqualTo("/handlesRetry")));
+    }
 
-  @Test
-  void usesGivenAcceptHeader() throws MalformedURLException {
-    mock.expectRequest("GET","/content.txt",
-        request -> request.withHeader("accept", "text/plain"),
-        response()
-            .withBody("Some text", MediaType.TEXT_PLAIN)
-    );
+    @Test
+    void usesGivenAcceptHeader(WireMockRuntimeInfo wmInfo) throws MalformedURLException {
+        stubFor(get("/content.txt")
+            .withHeader("accept", equalTo("text/plain"))
+            .willReturn(aResponse().withBody("Some text"))
+        );
 
-    final StringWriter output = new StringWriter();
-    final int status =
-        new CommandLine(new GetCommand())
-            .setOut(new PrintWriter(output))
-            .execute(
-                "--accept", "text/plain",
-                mock.buildMockedUrl("/content.txt").toString()
-            );
+        final StringWriter output = new StringWriter();
+        final int status =
+            new CommandLine(new GetCommand())
+                .setOut(new PrintWriter(output))
+                .execute(
+                    "--accept", "text/plain",
+                    wmInfo.getHttpBaseUrl() + "/content.txt"
+                );
 
-    assertThat(status).isEqualTo(0);
-    assertThat(output.toString()).isEqualTo("Some text");
-  }
+        assertThat(status).isEqualTo(0);
+        assertThat(output.toString()).isEqualTo("Some text");
+    }
 
-  @Test
-  void usesGivenApiKeyHeader() throws MalformedURLException {
-    mock.expectRequest("GET","/content.txt",
-        request -> request.withHeader("x-api-key", "xxxxxx"),
-        response()
-            .withBody("Some text", MediaType.TEXT_PLAIN)
-    );
+    @Test
+    void usesGivenApiKeyHeader(WireMockRuntimeInfo wmInfo) throws MalformedURLException {
+        stubFor(get("/content.txt")
+            .withHeader("x-api-key", equalTo("xxxxxx"))
+            .willReturn(aResponse().withBody("Some text"))
+        );
 
-    final StringWriter output = new StringWriter();
-    final int status =
-        new CommandLine(new GetCommand())
-            .setOut(new PrintWriter(output))
-            .execute(
-                "--apikey", "xxxxxx",
-                mock.buildMockedUrl("/content.txt").toString()
-            );
+        final StringWriter output = new StringWriter();
+        final int status =
+            new CommandLine(new GetCommand())
+                .setOut(new PrintWriter(output))
+                .execute(
+                    "--apikey", "xxxxxx",
+                    wmInfo.getHttpBaseUrl() + "/content.txt"
+                );
 
-    assertThat(status).isEqualTo(0);
-    assertThat(output.toString()).isEqualTo("Some text");
-  }
+        assertThat(status).isEqualTo(0);
+        assertThat(output.toString()).isEqualTo("Some text");
+    }
 
+    @Test
+    void tryHttps(WireMockRuntimeInfo wmInfo) throws Exception {
+        stubFor(get("/content.txt")
+            .willReturn(aResponse().withBody("Content"))
+        );
 
+        final String err = tapSystemErrNormalized(() -> {
+            final int exitCode = new CommandLine(new GetCommand())
+                .execute(
+                    wmInfo.getHttpsBaseUrl() + "/content.txt"
+                );
+
+            assertThat(exitCode).isEqualTo(ExitCode.SOFTWARE);
+        });
+
+        // Confirms it got far enough to establish a connection and didn't like
+        // self-signed cert from WireMock. Really should add support in the get
+        // command for "accept all certs".
+        // See https://wiremock.org/docs/https/#common-https-issues
+        assertThat(err).contains("unable to find valid certification path to requested target");
+    }
 }
