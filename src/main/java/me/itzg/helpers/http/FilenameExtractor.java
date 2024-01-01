972b6d95c2c8ca7af6b82de29b7e9f061e272587
Alexander Städing <alexanderstaeding@gmail.com>
2024-01-01 14:02:44 +0100

    Fix indentation in FilenameExtractor

--- src/main/java/me/itzg/helpers/http/FilenameExtractor.java
+++ src/main/java/me/itzg/helpers/http/FilenameExtractor.java
@@ -24,69 +24,69 @@ public class FilenameExtractor {
     private static final Pattern RFC_2047_ENCODED = Pattern.compile("=\\?UTF-8\\?Q\\?(.+)\\?=");
     private static final Pattern RFC_5987_ENCODED = Pattern.compile("(UTF-8|ISO-8859-1)''(.+)");
 
-  private final LatchingUrisInterceptor interceptor;
+    private final LatchingUrisInterceptor interceptor;
 
-  public FilenameExtractor(LatchingUrisInterceptor interceptor) {
-    this.interceptor = Objects.requireNonNull(interceptor, "interceptor is required");
-  }
-
-  static String filenameFromContentDisposition(String headerValue) {
-      if (headerValue == null) {
-          return null;
-      }
-
-      final ContentType parsed = ContentType.parse(headerValue);
-      log.debug("Response has contentDisposition={}", headerValue);
-      final String filenameStar = parsed.getParameter("filename*");
-      final String filename = parsed.getParameter("filename");
-      if (filenameStar != null) {
-          final Matcher m = RFC_5987_ENCODED.matcher(filenameStar);
-          if (m.matches()) {
-              try {
-                  return Rfc5987Util.decode(m.group(2), m.group(1));
-              } catch (UnsupportedEncodingException e) {
-                  throw new GenericException("Failed to decode filename* from " + headerValue, e);
-              }
-          }
-          else {
-              return filenameStar;
-          }
-      }
-      else if (filename != null) {
-          final Matcher m = RFC_2047_ENCODED.matcher(filename);
-          if (m.matches()) {
-              return m.group(1);
-          }
-          else {
-              return filename;
-          }
-      }
-      else {
-          throw new GenericException("Unable to determine filename from header: " + headerValue);
-      }
-  }
-
-  public String extract(ClassicHttpResponse response) throws IOException, ProtocolException {
-    // Same as AbstractHttpClientResponseHandler
-    if (response.getCode() >= HttpStatus.SC_REDIRECTION) {
-      EntityUtils.consume(response.getEntity());
-      throw new HttpResponseException(response.getCode(), response.getReasonPhrase());
+    public FilenameExtractor(LatchingUrisInterceptor interceptor) {
+        this.interceptor = Objects.requireNonNull(interceptor, "interceptor is required");
     }
 
-    final Header contentDisposition = response
-        .getHeader("content-disposition");
+    static String filenameFromContentDisposition(String headerValue) {
+        if (headerValue == null) {
+            return null;
+        }
 
-    String filename = null;
-    if (contentDisposition != null) {
-      filename = filenameFromContentDisposition(contentDisposition.getValue());
-    }
-    if (filename == null) {
-      final String path = interceptor.getLastRequestedUri().getPath();
-      log.debug("Deriving filename from response path={}", path);
-      final int pos = path.lastIndexOf('/');
-      filename = path.substring(pos >= 0 ? pos + 1 : 0);
+        final ContentType parsed = ContentType.parse(headerValue);
+        log.debug("Response has contentDisposition={}", headerValue);
+        final String filenameStar = parsed.getParameter("filename*");
+        final String filename = parsed.getParameter("filename");
+        if (filenameStar != null) {
+            final Matcher m = RFC_5987_ENCODED.matcher(filenameStar);
+            if (m.matches()) {
+                try {
+                    return Rfc5987Util.decode(m.group(2), m.group(1));
+                } catch (UnsupportedEncodingException e) {
+                    throw new GenericException("Failed to decode filename* from " + headerValue, e);
+                }
+            }
+            else {
+                return filenameStar;
+            }
+        }
+        else if (filename != null) {
+            final Matcher m = RFC_2047_ENCODED.matcher(filename);
+            if (m.matches()) {
+                return m.group(1);
+            }
+            else {
+                return filename;
+            }
+        }
+        else {
+            throw new GenericException("Unable to determine filename from header: " + headerValue);
+        }
     }
 
-    return filename;
-  }
+    public String extract(ClassicHttpResponse response) throws IOException, ProtocolException {
+        // Same as AbstractHttpClientResponseHandler
+        if (response.getCode() >= HttpStatus.SC_REDIRECTION) {
+            EntityUtils.consume(response.getEntity());
+            throw new HttpResponseException(response.getCode(), response.getReasonPhrase());
+        }
+
+        final Header contentDisposition = response
+            .getHeader("content-disposition");
+
+        String filename = null;
+        if (contentDisposition != null) {
+            filename = filenameFromContentDisposition(contentDisposition.getValue());
+        }
+        if (filename == null) {
+            final String path = interceptor.getLastRequestedUri().getPath();
+            log.debug("Deriving filename from response path={}", path);
+            final int pos = path.lastIndexOf('/');
+            filename = path.substring(pos >= 0 ? pos + 1 : 0);
+        }
+
+        return filename;
+    }
 }
