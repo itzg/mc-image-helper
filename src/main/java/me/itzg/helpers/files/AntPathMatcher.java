84cf40f486d45ae5c0d85614125c960d6b71d827
Geoff Bourne <itzgeoff@gmail.com>
2024-03-09 12:00:03 -0600

    modrinth: support excluding specific overrides (#410)

--- src/main/java/me/itzg/helpers/files/AntPathMatcher.java
+++ src/main/java/me/itzg/helpers/files/AntPathMatcher.java
@@ -1,41 +1,30 @@
 package me.itzg.helpers.files;
 
+import java.nio.file.FileSystems;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
 import java.util.Collection;
-import java.util.regex.Pattern;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class AntPathMatcher {
 
-    private final Pattern regex;
+    private final List<PathMatcher> pathMatchers;
 
     public AntPathMatcher(Collection<String> patterns) {
-        this.regex = patterns != null && !patterns.isEmpty() ?
-            convertToRegex(patterns)
+        this.pathMatchers = patterns != null ?
+            patterns.stream()
+                .map(s -> FileSystems.getDefault().getPathMatcher("glob:" + s))
+                .collect(Collectors.toList())
             : null;
     }
 
-    private static Pattern convertToRegex(Collection<String> patterns) {
-        return Pattern.compile(
-            patterns.stream()
-                .map(s ->
-                    // swap these out temporarily to avoid stepping on each other
-                    s.replace("**", "_DSTAR_")
-                        .replace("*", "_STAR_")
-                        // escape special characters
-                        .replaceAll("[.(\\[]", "\\\\$0")
-                        .replace("?", ".")
-                        // ...and then turn into regex equivalent
-                        .replace("_DSTAR_", ".*?")
-                        .replace("_STAR_", "[^/]*?")
-                )
-                // ...and join the whole thing into alternates to end up with one regex to match
-                // all the requested patterns
-                .collect(Collectors.joining("|"))
-        );
-    }
-
     public boolean matches(String input) {
-        return regex != null
-            && regex.matcher(input).matches();
+        return pathMatchers != null &&
+            (
+                pathMatchers.isEmpty() ||
+                    pathMatchers.stream()
+                        .anyMatch(pathMatcher -> pathMatcher.matches(Paths.get(input)))
+            );
     }
 }
