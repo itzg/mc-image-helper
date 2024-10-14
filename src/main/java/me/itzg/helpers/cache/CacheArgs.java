dd26d6aa42e79f195e68f617845723d993bbc878
Geoff Bourne <itzgeoff@gmail.com>
2024-10-14 18:16:01 -0500

    Add option descriptions

--- src/main/java/me/itzg/helpers/cache/CacheArgs.java
+++ src/main/java/me/itzg/helpers/cache/CacheArgs.java
@@ -7,9 +7,13 @@ import picocli.CommandLine.Option;
 
 @Data
 public class CacheArgs {
-    @Option(names = "--api-cache-ttl")
+    @Option(names = "--api-cache-ttl", paramLabel = "OPERATION=DURATION",
+        description = "Set individual operation TTLs"
+    )
     Map<String, Duration> cacheDurations;
 
-    @Option(names = "--api-cache-default-ttl", defaultValue = "P2D")
+    @Option(names = "--api-cache-default-ttl", defaultValue = "P2D", paramLabel = "DURATION",
+        description = "Set default/fallback TTL in ISO-8601 duration format.\nDefault: ${DEFAULT-VALUE}"
+    )
     Duration defaultCacheDuration;
 }
