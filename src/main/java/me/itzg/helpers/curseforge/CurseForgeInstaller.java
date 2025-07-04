15206e935ab60f9c83ac16492a05eba33f488499
Geoff Bourne <itzgeoff@gmail.com>
2025-07-04 08:40:34 -0500

    cf: retry file download on premature close error (#591)
    Also
    * cf: switch to backoff delay

--- src/main/java/me/itzg/helpers/curseforge/CurseForgeInstaller.java
+++ src/main/java/me/itzg/helpers/curseforge/CurseForgeInstaller.java
@@ -65,6 +65,7 @@ import org.jetbrains.annotations.Nullable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
+import reactor.netty.http.client.PrematureCloseException;
 import reactor.util.retry.Retry;
 
 @RequiredArgsConstructor
@@ -753,10 +754,11 @@ public class CurseForgeInstaller {
             )
             // retry the deferred part above if one of the expected failure cases
             .retryWhen(
-                Retry.fixedDelay(BAD_FILE_ATTEMPTS, BAD_FILE_DELAY)
+                Retry.backoff(BAD_FILE_ATTEMPTS, BAD_FILE_DELAY)
                     .filter(throwable ->
                         throwable instanceof FileHashInvalidException ||
-                            throwable instanceof FailedRequestException
+                            throwable instanceof FailedRequestException ||
+                            throwable instanceof PrematureCloseException
                     )
                     .doBeforeRetry(retrySignal ->
                         log.warn("Retrying to download {} @ {}:{}",
