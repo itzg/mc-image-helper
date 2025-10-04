66f92547045392abc7cf3d43927344bd560fbfd4
Geoff Bourne <itzgeoff@gmail.com>
2025-10-04 16:16:56 -0500

    paper: now applies channel and resolves stable by default

--- src/main/java/me/itzg/helpers/paper/RequestedChannel.java
+++ src/main/java/me/itzg/helpers/paper/RequestedChannel.java
@@ -1,6 +1,19 @@
 package me.itzg.helpers.paper;
 
+import lombok.Getter;
+import lombok.ToString;
+import me.itzg.helpers.paper.model.Channel;
+
+@ToString
+@Getter
 public enum RequestedChannel {
-    DEFAULT,
-    EXPERIMENTAL
+    DEFAULT(Channel.STABLE, Channel.RECOMMENDED),
+    EXPERIMENTAL(Channel.ALPHA, Channel.BETA),
+    ALPHA(Channel.ALPHA);
+
+    private final Channel[] mappedTo;
+
+    RequestedChannel(Channel... mappedTo) {
+        this.mappedTo = mappedTo;
+    }
 }
