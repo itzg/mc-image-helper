5663f239abf582e17a4209a37922ad0c7db8e537
Jack Hicks <jack.hicks@onebigcircle.co.uk>
2025-11-02 22:53:41 +0000

    test(ManifestsTest): test allFilesPresent

--- src/test/java/me/itzg/helpers/files/ManifestsTest.java
+++ src/test/java/me/itzg/helpers/files/ManifestsTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Collections;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
@@ -13,14 +14,13 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
-class ManifestsTest {
+public class ManifestsTest {
 
     @TempDir
     Path tempDir;
 
     @Getter @SuperBuilder @Jacksonized
     static class EmptyManifest extends BaseManifest {
-
     }
 
     @Test
@@ -32,5 +32,40 @@ class ManifestsTest {
 
         final EmptyManifest manifest = Manifests.load(tempDir, id, EmptyManifest.class);
         assertThat(manifest).isNull();
+    }
+
+    @Test
+    void allFilesPresent_withWildcardIgnoreAll() {
+        EmptyManifest manifest = EmptyManifest.builder()
+            .files(Arrays.asList("a.jar", "b.jar"))
+            .build();
+
+        boolean result = Manifests.allFilesPresent(tempDir, manifest, Collections.singletonList("*"));
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void allFilesPresent_withGlobPattern() throws IOException {
+        Files.createDirectories(tempDir.resolve("mods"));
+        Files.createFile(tempDir.resolve("mods/present.jar"));
+
+        EmptyManifest manifest = EmptyManifest.builder()
+            .files(Arrays.asList("mods/present.jar", "mods/missing.jar"))
+            .build();
+
+        boolean result = Manifests.allFilesPresent(tempDir, manifest, Collections.singletonList("mods/*.jar"));
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void allFilesPresent_withExplicitFileNames() throws IOException {
+        Files.createFile(tempDir.resolve("keep.jar"));
+
+        EmptyManifest manifest = EmptyManifest.builder()
+            .files(Arrays.asList("keep.jar", "remove.jar"))
+            .build();
+
+        boolean result = Manifests.allFilesPresent(tempDir, manifest, Collections.singletonList("remove.jar"));
+        assertThat(result).isTrue();
     }
 }
