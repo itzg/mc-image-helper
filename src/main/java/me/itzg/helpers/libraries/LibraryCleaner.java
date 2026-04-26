871e5822c45d0dc4be24c66b7f7a12a262ddbc4d
Harrison Black <dev+github@harrison.black>
2026-04-26 14:39:10 +1000

    fix(serverLibraryCleanUp): Wrap resources with try-with-resources
    Ensure closure of resources after accessing

--- src/main/java/me/itzg/helpers/libraries/LibraryCleaner.java
+++ src/main/java/me/itzg/helpers/libraries/LibraryCleaner.java
@@ -1,7 +1,6 @@
 package me.itzg.helpers.libraries;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -95,39 +94,34 @@ public class LibraryCleaner {
     private List<String> readJarLibraries(Path jarFile, LibraryListPaths libraryListPath) throws IOException {
         List<String> libs = new ArrayList<String>();
 
-        JarFile serverJar;
+        try (JarFile serverJar = new JarFile(jarFile.toString());) {
 
-        try {
-            serverJar = new JarFile(jarFile.toString());
+            JarEntry libraryList = serverJar.getJarEntry(libraryListPath.getPath());
+
+            if (libraryList == null) {
+                log.error("Failed to read library list {} for server type {}", libraryListPath.getPath(),
+                        libraryListPath.name());
+                return libs;
+            }
+
+            try (
+                    InputStream is = serverJar.getInputStream(libraryList);
+                    InputStreamReader isr = new InputStreamReader(is);
+                    BufferedReader br = new BufferedReader(isr);) {
+
+                libs = br.lines()
+                        .map(String::trim)
+                        .map(s -> s.split("\\s+"))
+                        .filter(parts -> parts.length > 1)
+                        .map(parts -> parts[2])
+                        .collect(Collectors.toList());
+
+            }
+
         } catch (Exception e) {
             log.error("Failed to read server jar {} {}", jarFile.toString(), e);
-            return libs;
         }
 
-        JarEntry libraryList = serverJar.getJarEntry(libraryListPath.getPath());
-
-        if (libraryList == null) {
-            log.error("Failed to read library List {} for server type {}", libraryListPath.getPath(),
-                    libraryListPath.name());
-            serverJar.close();
-            return libs;
-        }
-
-        try (
-                InputStream is = serverJar.getInputStream(libraryList);
-                InputStreamReader isr = new InputStreamReader(is);
-                BufferedReader br = new BufferedReader(isr);) {
-
-            libs = br.lines()
-                    .map(String::trim)
-                    .map(s -> s.split("\\s+"))
-                    .filter(parts -> parts.length > 1)
-                    .map(parts -> parts[2])
-                    .collect(Collectors.toList());
-
-        }
-
-        serverJar.close();
         return libs;
     }
 
