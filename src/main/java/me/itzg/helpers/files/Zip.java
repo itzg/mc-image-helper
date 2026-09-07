404b1c2bacc2474d7be65cf356eef3d76940ab4c
Harrison Black <dev+github@harrison.black>
2026-09-07 14:55:02 +1000

    feat: Add Zip validate to check Zip Slips
    Adds static helper function to check existance of zip slips inside zip

---
+++ src/main/java/me/itzg/helpers/files/Zip.java
@@ -0,0 +1,29 @@
+package me.itzg.helpers.files;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class Zip {
+
+    static boolean validate(Path zip) throws IOException, SecurityException {
+        final Path extractionRoot = zip.toAbsolutePath().normalize().getParent();
+
+        try (ZipInputStream zis = new ZipInputStream(Files.newInputStream(zip))) {
+            ZipEntry entry;
+
+            while ((entry = zis.getNextEntry()) != null) {
+                final Path output = extractionRoot.resolve(entry.getName()).normalize();
+
+                if (!output.startsWith(extractionRoot)) {
+                    throw new SecurityException(
+                            "Zip Slip detected at: " + entry.getName() + " in zip: " + zip.toAbsolutePath());
+                }
+            }
+        }
+
+        return true;
+    }
+}
