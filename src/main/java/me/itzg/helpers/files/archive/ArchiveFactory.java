3d00389d674c4e1ad36e5c78d55f53d7705d7989
Harrison Black <dev+github@harrison.black>
2026-09-12 13:57:32 +1000

    refactor: Replace subclass instantiation with TarArchive and input stream opener

--- src/main/java/me/itzg/helpers/files/archive/ArchiveFactory.java
+++ src/main/java/me/itzg/helpers/files/archive/ArchiveFactory.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
+import org.apache.commons.compress.compressors.zstandard.ZstdCompressorInputStream;
+
 public final class ArchiveFactory {
 
     static Archive create(Path archive) throws IOException {
@@ -21,12 +25,11 @@ public final class ArchiveFactory {
 
         return switch (contentType) {
             case "application/zip" -> new Zip(archive);
-            case "application/x-tar" -> new Tar(archive);
-            case "application/gzip", "application/x-gzip" -> new TarGzip(archive);
-            case "application/x-bzip2", "application/bz2", "application/x-bzip" -> new TarBzip2(archive);
-            case "application/zstd", "application/x-zstd" -> new TarZstd(archive);
+            case "application/x-tar" -> new TarArchive(archive, Files::newInputStream);
+            case "application/gzip", "application/x-gzip" -> new TarArchive(archive, path -> new GzipCompressorInputStream(Files.newInputStream(path)));
+            case "application/x-bzip2", "application/bz2", "application/x-bzip" -> new TarArchive(archive, path -> new BZip2CompressorInputStream(Files.newInputStream(path)));
+            case "application/zstd", "application/x-zstd" -> new TarArchive(archive, path -> new ZstdCompressorInputStream(Files.newInputStream(path)));
             default -> throw new IOException("Failed to read file MIME type: " + archive.toAbsolutePath());
         };
     }
-
 }
