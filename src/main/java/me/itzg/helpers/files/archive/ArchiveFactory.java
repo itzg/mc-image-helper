17861e07031978617a571edb62c27d5a79aee01f
Harrison Black <dev+github@harrison.black>
2026-09-21 15:13:05 +1000

    refactor: Update ArchiveFactory with new instantiation

--- src/main/java/me/itzg/helpers/files/archive/ArchiveFactory.java
+++ src/main/java/me/itzg/helpers/files/archive/ArchiveFactory.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.commons.compress.compressors.zstandard.ZstdCompressorInputStream;
@@ -24,11 +26,14 @@ public final class ArchiveFactory {
         }
 
         return switch (contentType) {
-            case "application/zip" -> new Zip(archive);
-            case "application/x-tar" -> new TarArchive(archive, Files::newInputStream);
-            case "application/gzip", "application/x-gzip" -> new TarArchive(archive, path -> new GzipCompressorInputStream(Files.newInputStream(path)));
-            case "application/x-bzip2", "application/bz2", "application/x-bzip" -> new TarArchive(archive, path -> new BZip2CompressorInputStream(Files.newInputStream(path)));
-            case "application/zstd", "application/x-zstd" -> new TarArchive(archive, path -> new ZstdCompressorInputStream(Files.newInputStream(path)));
+            case "application/zip" -> new Archive(archive, ZipArchiveInputStream::new);
+            case "application/x-tar" -> new Archive(archive, TarArchiveInputStream::new);
+            case "application/gzip", "application/x-gzip" -> new Archive(archive,
+                    input -> new TarArchiveInputStream(new GzipCompressorInputStream(input)));
+            case "application/x-bzip2", "application/bz2", "application/x-bzip" -> new Archive(archive,
+                    input -> new TarArchiveInputStream(new BZip2CompressorInputStream(input)));
+            case "application/zstd", "application/x-zstd" -> new Archive(archive,
+                    input -> new TarArchiveInputStream(new ZstdCompressorInputStream(input)));
             default -> throw new IOException("Failed to read file MIME type: " + archive.toAbsolutePath());
         };
     }
